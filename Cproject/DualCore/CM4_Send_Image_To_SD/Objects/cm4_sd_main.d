046f09535d50301c73baa52f9fed9c2a722ba73a
SwedishHouse <93093205+SwedishHouse@users.noreply.github.com>
2023-02-14 12:36:11 +0400

    added project for writting to sd

---
+++ Cproject/DualCore/CM4_Send_Image_To_SD/Objects/cm4_sd_main.d
@@ -0,0 +1,23 @@
+./objects/cm4_sd_main.o: cm4_sd_main.c \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\Device\Include\LPC43xx.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\Device\Include\system_LPC43xx.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_SPI.h \
+  C:\Keil_packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_Common.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\SSP_LPC43xx.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\SCU_LPC43xx.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\GPIO_LPC43xx.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\GPDMA_LPC43xx.h \
+  RTE\Device\LPC4357_Cortex-M4\RTE_Device.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\GPDMA_LPC43xx.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\GPIO_LPC43xx.h \
+  C:\Keil_packs\Keil\LPC4300_DFP\2.10.0\RTE_Driver\SCU_LPC43xx.h
---
+++ Cproject/DualCore/CM4_Send_Image_To_SD/cm4_sd_main.c
@@ -0,0 +1,18 @@
+#include "LPC43xx.h"                    // Device header
+#include "RTE_Components.h"             // Component selection
+#include "Driver_SPI.h"                 // ::CMSIS Driver:SPI
+#include "SSP_LPC43xx.h"                // Keil::CMSIS Driver:SPI:SSP
+#include "GPDMA_LPC43xx.h"              // Keil::Device:GPDMA
+#include "GPIO_LPC43xx.h"               // Keil::Device:GPIO
+#include "SCU_LPC43xx.h"                // Keil::Device:SCU
+#include "RTE_Device.h"                 // Keil::Device:Startup
+
+
+int main(void)
+{
+	while(1)
+	{
+		
+	}
+}
+
